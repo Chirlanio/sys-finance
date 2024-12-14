caf39c720e2fd338f05816e3149436e4d2097396
Chirlanio <56799352+Chirlanio@users.noreply.github.com>
2024-12-14 17:01:03 -0300

    Page login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/Login";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import image from "../../assets/login-animate.svg";
+
+export default function LoginPage() {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
+  function clickLogin() {
+    console.log(user);
+    console.log(password);
+  }
+
+  return (
+    <div className="w-full h-screen bg-[#1e1e1e] text-white flex">
+      <div className="w-[50%] h-full flex justify-center items-center flex-col">
+        <div className="w-full h-[20%] flex justify-center items-center">
+          <h1 className="text-[30px] font-bold text-[#cccccc]">
+            Faça o login
+            <br />E entre para o nosso time!
+          </h1>
+        </div>
+        <div className="w-full h-[80%]">
+          <img src={image} alt="image" width={550} />
+        </div>
+      </div>
+      <div className="w-[50%] h-full flex justify-center items-center">
+        <div className="w-[60%] h-[50%] bg-[#292929] rounded-xl">
+          <div className="w-full h-[20%] flex items-center justify-center">
+            <h1 className="text-[#cccccc] font-bold text-[30px]">LOGIN</h1>
+          </div>
+          <div className="w-full h-[60%] flex items-center flex-col gap-5">
+            <div className="w-[80%] flex flex-col">
+              <label className="text-[16px]" htmlFor="">
+                Usuário
+              </label>
+              <input
+                className="rounded-lg border-none w-full h-[35px] text-[16px] bg-[#eeeeee] p-2 outline-none text-[#000000]"
+                placeholder="Digite o seu usuário..."
+                type="text"
+                onChange={(e) => setUser(e.target.value)}
+              />
+            </div>
+            <div className="w-[80%] flex flex-col">
+              <label className="text-[16px]" htmlFor="">
+                Senha
+              </label>
+              <input
+                className="rounded-lg border-none w-full h-[35px] text-[16px] bg-[#eeeeee] p-2 outline-none text-[#000000]"
+                placeholder="Digite sua senha"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="w-full flex items-center justify-center leading-[0]">
+              <a href="#" className="text-[10px] hover:underline decoration-1">
+                Recupere sua senha
+              </a>
+            </div>
+          </div>
+          <div className="w-full h-[20%] flex justify-center">
+            <button
+              onClick={clickLogin}
+              className="w-[80%] h-[35px] rounded-lg bg-[#cccccc] text-[#1e1e1e] font-bold hover:bg-[#969595]"
+            >
+              Acessar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
